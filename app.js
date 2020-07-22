8ed2df84ab88e2ce81718fe1ab1d927a55f4a204
Soochin Kang <soochink@uci.edu>
2020-07-22 16:28:37 -0700

    add start script

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("The YelpCamp Server has started!");
 });
